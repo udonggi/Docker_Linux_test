51c1f4a41abc1e26dffc84578eeb35ee529be72b
udonggi <dongki1882@naver.com>
2023-04-24 13:22:02 +0900

    Test docker

---
+++ src/main/java/com/ll/dockerTest/MainController.java
@@ -0,0 +1,15 @@
+package com.ll.dockerTest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String main() {
+        return "Hello, Docker!";
+    }
+}
